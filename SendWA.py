dc2411675c2e04232e39091cb6607f53c35aa3a2
moby989 <46134028+moby989@users.noreply.github.com>
2020-11-13 13:01:30 +0300

    Update SendWA.py

--- SendWA.py
+++ SendWA.py
@@ -24,8 +24,11 @@ from pymongo.errors import DuplicateKeyError
 TWILIO_ACCOUNT_SID='AC50db7eab8549659741f0587cece1f3f6'
 TWILIO_AUTH_TOKEN='4b42156d82a8df03dc2cf8799e2955b0'
 
+url_cian_z = 'https://www.cian.ru/cat.php?currency=2&deal_type=rent&engine_version=2&highway%5B0%5D=9&highway%5B1%5D=24&highway%5B2%5D=27&highway%5B3%5D=28&highway%5B4%5D=44&highway%5B5%5D=19&maxmcad=25&maxprice=80000&minarea=80&object_type%5B0%5D=1&object_type%5B1%5D=2&object_type%5B2%5D=4&offer_type=suburban&sort=creation_date_desc&totime=-2&type=4'
+url_cian_yz = 'https://www.cian.ru/cat.php?currency=2&deal_type=rent&engine_version=2&highway%5B0%5D=12&highway%5B1%5D=10&maxmcad=40&maxprice=80000&minarea=80&object_type%5B0%5D=1&object_type%5B1%5D=2&object_type%5B2%5D=4&offer_type=suburban&sort=creation_date_desc&totime=-2&type=4'
+url_cian = {'Западное':url_cian_z,'Юго-Западное':url_cian_yz}
 
-url_cian = 'https://www.cian.ru/cat.php?currency=2&deal_type=rent&engine_version=2&highway%5B0%5D=49&highway%5B10%5D=129&highway%5B11%5D=10&highway%5B12%5D=29&highway%5B13%5D=5&highway%5B14%5D=40&highway%5B15%5D=19&highway%5B16%5D=2&highway%5B17%5D=12&highway%5B18%5D=42&highway%5B19%5D=41&highway%5B1%5D=9&highway%5B2%5D=16&highway%5B3%5D=17&highway%5B4%5D=24&highway%5B5%5D=27&highway%5B6%5D=28&highway%5B7%5D=44&highway%5B8%5D=30&highway%5B9%5D=4&maxmcad=50&maxprice=80000&minarea=80&object_type%5B0%5D=1&object_type%5B1%5D=2&object_type%5B2%5D=4&offer_type=suburban&sort=creation_date_desc&totime=-2&type=4'
+#url_cian = 'https://www.cian.ru/cat.php?currency=2&deal_type=rent&engine_version=2&highway%5B0%5D=49&highway%5B10%5D=129&highway%5B11%5D=10&highway%5B12%5D=29&highway%5B13%5D=5&highway%5B14%5D=40&highway%5B15%5D=19&highway%5B16%5D=2&highway%5B17%5D=12&highway%5B18%5D=42&highway%5B19%5D=41&highway%5B1%5D=9&highway%5B2%5D=16&highway%5B3%5D=17&highway%5B4%5D=24&highway%5B5%5D=27&highway%5B6%5D=28&highway%5B7%5D=44&highway%5B8%5D=30&highway%5B9%5D=4&maxmcad=50&maxprice=80000&minarea=80&object_type%5B0%5D=1&object_type%5B1%5D=2&object_type%5B2%5D=4&offer_type=suburban&sort=creation_date_desc&totime=-2&type=4'
 url_avito = 'https://www.avito.ru/moskva_i_mo/doma_dachi_kottedzhi/sdam/na_dlitelnyy_srok-ASgBAgICAkSUA9IQoAjIVQ?cd=1&f=ASgBAQECAkSUA9IQoAjIVQFA2gg01FnSWdZZAkWSCRl7ImZyb20iOjE0NTcxLCJ0byI6MTQ1NzV9whMYeyJmcm9tIjpudWxsLCJ0byI6MTQ2Nzd9&pmax=80000&pmin=40000&proprofile=1&road=2-4-5-9-10-11-12-15-16-18-23-25-26-28-29&s=104'
 
 # this is the Twilio sandbox testing number
